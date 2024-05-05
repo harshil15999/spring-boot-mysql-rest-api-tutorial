a4f4f8023ddc6060b0bc33e16c992742b65dd36c
harshil shah <harshilfshah15999@gmail.com>
2024-05-05 13:11:49 +0530

    Feature: Adding logging in IndexController routes

--- src/main/java/com/example/easynotes/controller/IndexController.java
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -1,16 +1,25 @@
 package com.example.easynotes.controller;
-
-
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class IndexController {
 
     @GetMapping
     public String sayHello() {
-        return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+        try{
+            log.info("/ API hit");
+            return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+        } catch (Exception e ){
+            log.error("Error:{}",e);
+            return "Contact Administrator";
+        }
+
     }
 }
